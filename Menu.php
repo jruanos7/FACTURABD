55feba0481f3f4e541d852ad99f2058f95ad6366
Jasmine <jruanos5@miumg.edu.gt>
2026-02-19 00:28:19 -0600

    Colocamos imagen en menú

--- Menu.php
+++ Menu.php
@@ -1,10 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        body {
+            background-image: url("fondo.png");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-color: rgba(0, 0, 0, 0.5);
+            background-blend-mode: darken;
+        }
+    </style>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -14,17 +27,17 @@
 <body>
 
     <div class="container vh-100 d-flex justify-content-center align-items-center">
-        <div class="bg-white p-4 shadow rounded">
+        <div class="bg-dark text-white p-4 shadow rounded">
 
 
 
-            <h1 class="mb-5 container d-flex justify-content-center align-items-center">BIENVENIDO</h1>
+            <h1 class="mb-3 container d-flex justify-content-center align-items-center">BIENVENIDO</h1>
             <div class="d-grid gap-0.5 d-flex justify-content-center">
-                <a href="Cliente.php"> <button class="btn bg-warning mx-2">Cliente</button></a>
+                <a href="Cliente.php"> <button style="background-color: #6f42c1;" class="mx-2">Cliente</button></a>
                 <br>
-                <a href="Producto.php"> <button class="btn bg-danger mx-2">Producto</button></a>
+                <a href="Producto.php"> <button style="background-color: #6f42c1;" class="mx-2">Producto</button></a>
                 <br>
-                <a href="Venta.php"> <button class="btn btn-info mx-2">Venta</button></a>
+                <a href="Venta.php"> <button style="background-color: #6f42c1;" class="mx-2">Venta</button></a>
             </div>
         </div>
     </div>
